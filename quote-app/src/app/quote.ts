2bafd9f738c90ede6f6f06cf86afb8a736fb533e
karanikathungu-crypto <kathungudev@gmail.com>
2021-08-22 19:21:39 +0300

    Add boolean type variable to hide quote details

--- quote-app/src/app/quote.ts
+++ quote-app/src/app/quote.ts
@@ -1,6 +1,7 @@
 export class Quote {
+  displayDescription:boolean;
   constructor(public id: number,public author: string,public message: string,public poster: string,public elapsedTime: Date){
-
+    this.displayDescription= false;
   }
 }
 
