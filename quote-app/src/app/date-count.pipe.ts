94439ea5a789127f1462e7dfbecd2f4150e0423d
karanikathungu-crypto <kathungudev@gmail.com>
2021-08-23 17:09:29 +0300

    Add CSS styles in various external stylesheets

--- quote-app/src/app/date-count.pipe.ts
+++ quote-app/src/app/date-count.pipe.ts
@@ -5,8 +5,22 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class DateCountPipe implements PipeTransform {
 
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
+  transform(value: any):number{
+    let today: Date = new Date();
+    let todayWithoutTime:any = new Date(today.getFullYear(), today.getMonth(), today.getDate())
+    var dateDifference = Math.abs(value - todayWithoutTime)
+    const secondsEachDay = 86400;
+    var dateDifferenceInSeconds = dateDifference*0.001;
+    var dateCounter = dateDifferenceInSeconds/secondsEachDay;
+
+    if(dateCounter >= 1 && value > todayWithoutTime){
+      return dateCounter;
+    }
+    else{
+      return 0;
+        }
+    }
+
+  
   }
 
-}
